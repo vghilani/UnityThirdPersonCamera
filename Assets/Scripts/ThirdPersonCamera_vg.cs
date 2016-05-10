12ace1527c6c63cf8f0b331a49b586f20fab1c7c
vghilani <vghilani@gmail.com>
2016-05-09 21:58:16 -0500

    CameraFollow_lvl1

---
+++ Assets/Scripts/ThirdPersonCamera_vg.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera_vg : MonoBehaviour {
+
+	#region Variables (private)
+
+	[SerializeField]
+	private float distanceAway;
+	[SerializeField]
+	private float distanceUp;
+	[SerializeField]
+	private float smooth;
+	[SerializeField]
+	private Transform follow;
+	private Vector3 targetPosition;
+
+	#endregion
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		follow = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LateUpdate() {
+		
+		targetPosition = follow.position + follow.up * distanceUp - follow.forward * distanceAway;
+		Debug.DrawRay (follow.position, Vector3.up * distanceUp, Color.red);
+		Debug.DrawRay(follow.position, -1f * follow.forward * distanceAway, Color.blue);
+		Debug.DrawLine(follow.position, targetPosition, Color.magenta);
+
+		transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * smooth);
+
+		transform.LookAt (follow);
+
+	}
+
+
+}
